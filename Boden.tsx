d166f101f091ebe9d032e21efde64d1f1bf0bf1e
nep38837 <76069872+nep38837@users.noreply.github.com>
2020-12-17 21:45:59 +0100

    Create Boden.tsx

---
+++ Boden.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Boden" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="../weihnachtsfeier_ucs/tileset_Boden_0712.png" width="512" height="384"/>
+</tileset>
